5c124f88d2d8570c1543bb5ee5e51cb71f376984
Khalil Estell <kammcecorp@gmail.com>
2022-11-18 09:03:13 -0800

    Make WLAN + TCP Socket work

---
+++ demos/applications/wlan_client.cpp
@@ -0,0 +1,134 @@
+#include <libarmcortex/dwt_counter.hpp>
+#include <libesp8266/at/tcp_socket.hpp>
+#include <libesp8266/at/wlan_client.hpp>
+#include <libhal/serial/util.hpp>
+#include <libhal/steady_clock/util.hpp>
+#include <libhal/timeout.hpp>
+#include <liblpc40xx/constants.hpp>
+#include <liblpc40xx/system_controller.hpp>
+#include <liblpc40xx/uart.hpp>
+
+/**
+ * @brief Debug serial data on one serial port via another
+ *
+ * This class takes two serial ports, a "primary" and a "mirror". When the write
+ * and read APIs are used, data is passed directly through to the primary. On
+ * write operations, mirror will also write the same data. On read, the mirror
+ * will report the read data by also writing it to its port. This way, a user
+ * using a serial to USB debugger or some other debugging device can inspect
+ * what is being written to and read from the primary serial port.
+ *
+ */
+class serial_mirror : public hal::serial
+{
+public:
+  serial_mirror(hal::serial& p_primary, hal::serial& p_mirror)
+    : m_primary(&p_primary)
+    , m_mirror(&p_mirror)
+  {
+  }
+
+  hal::status driver_configure(const settings& p_settings) noexcept override
+  {
+    return m_primary->configure(p_settings);
+  }
+
+  hal::result<write_t> driver_write(
+    std::span<const hal::byte> p_data) noexcept override
+  {
+    HAL_CHECK(hal::write(*m_mirror, "WRITE:["));
+    HAL_CHECK(m_mirror->write(p_data));
+    HAL_CHECK(hal::write(*m_mirror, "]\n"));
+    return m_primary->write(p_data);
+  }
+
+  hal::result<read_t> driver_read(std::span<hal::byte> p_data) noexcept override
+  {
+    auto result = m_primary->read(p_data);
+
+    if (result.has_value() && result.value().data.size() != 0) {
+      HAL_CHECK(m_mirror->write(p_data));
+    }
+
+    return result;
+  }
+
+  hal::status driver_flush() noexcept override
+  {
+    return m_primary->flush();
+  }
+
+private:
+  hal::serial* m_primary;
+  hal::serial* m_mirror;
+};
+
+hal::status application()
+{
+  using namespace std::chrono_literals;
+  using namespace hal::literals;
+
+  HAL_CHECK(hal::lpc40xx::clock::maximum(12.0_MHz));
+
+  auto& clock = hal::lpc40xx::clock::get();
+  auto cpu_frequency = clock.get_frequency(hal::lpc40xx::peripheral::cpu);
+  hal::cortex_m::dwt_counter counter(cpu_frequency);
+
+  auto& uart0 = hal::lpc40xx::uart::get<0>(hal::serial::settings{
+    .baud_rate = 38400,
+  });
+
+  HAL_CHECK(hal::write(uart0, "ESP8266 WiFi Client Application Starting...\n"));
+  auto& uart3 = hal::lpc40xx::uart::get<3, 8192>();
+
+  serial_mirror mirror(uart3, uart0);
+  std::array<hal::byte, 8192> buffer{};
+
+  auto wlan_client_result = hal::esp8266::at::wlan_client::create(
+    mirror,
+    "KAMMCE-PHONE",
+    "roverteam",
+    HAL_CHECK(hal::create_timeout(counter, 30s)));
+
+  if (!wlan_client_result) {
+    HAL_CHECK(hal::write(uart0, "Failed to create wifi client!\n"));
+    return wlan_client_result.error();
+  }
+
+  auto wlan_client = wlan_client_result.value();
+  auto tcp_socket_result = hal::esp8266::at::tcp_socket::create(
+    wlan_client,
+    "example.com",
+    "80",
+    HAL_CHECK(hal::create_timeout(counter, 1s)));
+
+  if (!tcp_socket_result) {
+    HAL_CHECK(hal::write(uart0, "TCP Socket couldn't be established\n"));
+    return tcp_socket_result.error();
+  }
+
+  auto tcp_socket = std::move(tcp_socket_result.value());
+
+  while (true) {
+    static constexpr std::string_view get_request = "GET / HTTP/1.1\r\n"
+                                                    "Host: example.com:80\r\n"
+                                                    "\r\n";
+    buffer.fill('_');
+
+    HAL_CHECK(hal::write(uart0, "Sending:\n\n"));
+    HAL_CHECK(hal::write(uart0, get_request));
+    HAL_CHECK(hal::write(uart0, "\n\n"));
+
+    HAL_CHECK(tcp_socket.write(hal::as_bytes(get_request)));
+    HAL_CHECK(hal::delay(counter, 1000ms));
+
+    auto received = HAL_CHECK(tcp_socket.read(buffer)).data;
+
+    HAL_CHECK(hal::write(uart0, "================Full Response!===========\n"));
+    HAL_CHECK(hal::write(uart0, received));
+    HAL_CHECK(hal::write(uart0, "\n\n"));
+    HAL_CHECK(hal::write(uart0, "=================== /end ================\n"));
+  }
+
+  return hal::success();
+}
