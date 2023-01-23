84ee570a56aaf559b236bc00c50fb01d976b1e06
Khalil Estell <kammcecorp@gmail.com>
2023-01-23 13:51:22 -0800

    :arrow_up: Bump libhal version

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ required_conan_version = ">=1.50.0"
 
 class LibhalEsp8266Conan(ConanFile):
     name = "libhal-esp8266"
-    version = "0.3.3"
+    version = "0.3.4"
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/libhal/libhal-esp8266"
@@ -37,8 +37,8 @@ class LibhalEsp8266Conan(ConanFile):
         }
 
     def requirements(self):
-        self.requires("libhal/0.3.2@")
-        self.requires("libhal-util/0.3.3@")
+        self.requires("libhal/0.3.3@")
+        self.requires("libhal-util/0.3.7@")
 
     def validate(self):
         if self.settings.get_safe("compiler.cppstd"):
