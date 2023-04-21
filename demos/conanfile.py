4118f4193d2260f20309f3cfb77f1442bc398aa8
Khalil Estell <kammce@google.com>
2023-04-20 21:37:22 -0700

    :page_facing_up: Add appropriate licensing

--- demos/conanfile.py
+++ demos/conanfile.py
@@ -1,3 +1,17 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from conan import ConanFile
 from conan.tools.cmake import CMake, cmake_layout
 
@@ -8,7 +22,7 @@ class RmdDemos(ConanFile):
 
     def requirements(self):
         self.requires("libhal-lpc40xx/[^1.0.0]")
-        self.requires("libhal-esp8266/1.0.1")
+        self.requires("libhal-esp8266/1.0.2")
         self.tool_requires("gnu-arm-embedded-toolchain/11.3.0")
         self.tool_requires("cmake-arm-embedded/0.1.1")
 
