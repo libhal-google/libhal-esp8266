e710c2c38b8273bae30f83211d18d3725870aef9
Khalil Estell <kammce@google.com>
2023-04-22 07:40:03 -0700

    :green_heart: Fix ETL & lpc40 dependency
    It seems that the old libhal-lpc40xx dependency was causing issues with
    the etl library's cmake generation. The cause of this is unknown, but
    libhal-lpc40xx is deprecated so can be migrated away from at this time.

--- demos/conanfile.py
+++ demos/conanfile.py
@@ -16,12 +16,12 @@ from conan import ConanFile
 from conan.tools.cmake import CMake, cmake_layout
 
 
-class RmdDemos(ConanFile):
+class esp8266_demos(ConanFile):
     settings = "compiler", "build_type"
     generators = "CMakeToolchain", "CMakeDeps", "VirtualBuildEnv"
 
     def requirements(self):
-        self.requires("libhal-lpc40xx/[^1.0.0]")
+        self.requires("libhal-lpc40/[^1.0.0]")
         self.requires("libhal-esp8266/1.0.2")
         self.tool_requires("gnu-arm-embedded-toolchain/11.3.0")
         self.tool_requires("cmake-arm-embedded/0.1.1")
