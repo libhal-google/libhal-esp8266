55425f2aab746caab038a77b08f0db731bb8bcf1
Khalil Estell <kammce@google.com>
2023-06-25 08:15:12 -0700

    :white_check_mark: Add a test for at
    Remove old unused unit tests.

--- tests/at.test.cpp
+++ tests/at.test.cpp
@@ -1,8 +1,23 @@
 #include <libhal-esp8266/at.hpp>
 
+#include "helpers.hpp"
+
+#include <boost/ut.hpp>
+
 namespace hal::esp8266 {
 void at_test()
 {
-  // Fill this out later
+  using namespace boost::ut;
+
+  "at::create() + ::reset()"_test = []() {
+    using namespace std::literals;
+    // Setup
+    mock_serial mock;
+    mock.m_stream_out = stream_out("ready\r\n OK\r\n OK\r\n"sv);
+
+    // Exercise
+    // Verify
+    [[maybe_unused]] auto at = at::create(mock, hal::never_timeout()).value();
+  };
 }
 }  // namespace hal::esp8266
